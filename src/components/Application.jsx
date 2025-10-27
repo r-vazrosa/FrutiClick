023322f03810f88aa7435ad424e1f4bf9161fe66
r-vazrosa <rvazquezrosales8888@gmail.com>
2025-10-26 23:15:49 -0400

    image fix

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,18 +1,27 @@
 import '../styles/Application.css';
 
 
-const Application = ({ iconPath, name, openApp}) => (
-<div className="app">
-    <button
-      type="button"
-      className="app-button"
-      onClick={() => openApp && openApp(name)}
-      title={`Open ${name}`}>
-      <img src={iconPath} alt={name} />
-      <p>{name}</p>
-    </button>
-  </div>
-);
+const Application = ({ iconPath, name, openApp }) => {
+  const publicUrl = process.env.PUBLIC_URL || '';
+  const src =
+    iconPath.startsWith('http') || iconPath.startsWith(publicUrl)
+      ? iconPath
+      : `${publicUrl}${iconPath}`;
+
+  return (
+    <div className="app">
+      <button
+        type="button"
+        className="app-button"
+        onClick={() => openApp && openApp(name)}
+        title={`Open ${name}`}
+      >
+        <img src={src} alt={name} />
+        <p>{name}</p>
+      </button>
+    </div>
+  );
+};
 
 export default Application;
 
